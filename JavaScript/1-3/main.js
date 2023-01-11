9164026061abab6cdb4300db5690a06df190a821
DaisukeIto19930303 <105031221+DaisukeIto19930303@users.noreply.github.com>
2023-01-12 02:24:37 +0900

    1-3課題

---
+++ JavaScript/1-3/main.js
@@ -0,0 +1,18 @@
+let a = 9
+let b = 12
+console.log(a==10);
+console.log(b>=10 && b<20 &&b%2==0);
+
+let x = 10;
+if(10<=x && 20>=x){
+    console.log("成功です");
+}else{
+    console.log("失敗です");
+}
+
+let y = 0
+if(y%2==0){
+    console.log("偶数です");
+}else if(y%2!==0){
+    console.log("奇数です" );
+}
