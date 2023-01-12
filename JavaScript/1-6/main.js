fed3421c2ebd4a1a7f4f2c61ee087d4a21c32055
DaisukeIto19930303 <105031221+DaisukeIto19930303@users.noreply.github.com>
2023-01-13 01:04:34 +0900

    1-6課題

---
+++ JavaScript/1-6/main.js
@@ -0,0 +1,12 @@
+let scores = [10,15,20,25];
+console.log(scores);
+for (let i = 0; i < 4; i++) {
+    if (scores[i] % 2 === 0) {
+    console.log(scores[i]+'は偶数です');
+    }
+}
+
+//     let scores = [50, 60, 70, 80, 90];
+// for (let i = 0; i < 5; i++) {
+//     console.log("得点は" + scores[i] + "点です");
+// }
