e66b3b9bb3dfc0a9b2e85dc558e7acf4b3f58065
DaisukeIto19930303 <105031221+DaisukeIto19930303@users.noreply.github.com>
2023-01-13 01:38:01 +0900

    1-7課題

---
+++ JavaScript/1-7/main.js
@@ -0,0 +1,18 @@
+class Taiyaki {
+
+    //コンストラクタ
+    constructor(taste) {
+        this.taste = taste;
+}
+Tiyaki_type(){
+    console.log(`中身は${this.taste}です`);
+    }
+}
+//「山田」オブジェクト作成
+let anko = new Taiyaki('あんこ');
+let cream = new Taiyaki('クリーム');
+let cheese = new Taiyaki('チーズ');
+
+anko.Tiyaki_type();
+cream.Tiyaki_type();
+cheese.Tiyaki_type();
